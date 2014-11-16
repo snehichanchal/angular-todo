7b69bc231f9e3c268f75c2354a62045c170cd53a
Snehi Chanchal <snehichanchal92@gmail.com>
2014-11-17 01:07:46 +0530

    Files added

---
+++ app/scripts/app.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name todoApp
+ * @description
+ * # todoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('todoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'LocalStorageModule'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .when('/todo',{
+        templateUrl: 'views/todo.html',
+        controller: 'MyCtrl'
+      })
+      .otherwise({
+        redirectTo: '/todo'
+      });
+  });
---
+++ app/scripts/controllers/todo.js
@@ -0,0 +1,70 @@
+'use strict'
+
+angular.module('todoApp')
+  .controller("MyCtrl",function($scope,$log){
+  	//$scope.toggleComplete=false;
+  	//$scope.editorEnabled=false;
+	var local = localStorage.getItem("todoList");
+	if(local) {
+		$scope.todos=JSON.parse(local);					
+	} 
+	else {
+		$scope.todos=[];
+	}
+	$scope.saveEdited = function() {
+		$log.info("hello");
+		localStorage.setItem("todoList", JSON.stringify($scope.todos));
+
+	}
+	$scope.addItem=function(){
+		if($scope.item){
+		$scope.todos.push({'title':$scope.item, 'completed':false});
+		$scope.item='';
+		}
+		//$log.info($scope.todos);
+		localStorage.setItem("todoList", JSON.stringify($scope.todos));
+		
+	};
+
+	$scope.RemoveItem=function(title){
+		for(var i=$scope.todos.length-1;i >=0;i--){
+			if($scope.todos[i].title==title){
+				
+				//$log.info($scope.todos[i]);
+				//$log.info($scope.todos);
+				var index = $scope.todos.indexOf($scope.todos[i]);
+				$scope.todos.splice(index,1);
+
+			}
+
+		}
+		localStorage.setItem("todoList", JSON.stringify($scope.todos));
+		//$scope.todos.pop(title);  
+	};
+	$scope.onComplete=function(title, direction){
+		//$log.info($scope.todos.length)
+		//$log.info($scope.todos)
+		for(var i=$scope.todos.length-1;i >=0;i--)		
+		{
+			//$log.info($scope.todos[i].completed)
+			if($scope.todos[i].title==title){
+				$scope.todos[i].completed=direction;
+			}
+		}
+		localStorage.setItem("todoList", JSON.stringify($scope.todos));
+	};
+	// $scope.onIncomplete=function(title){
+
+	// 	for(var i=$scope.todos.length-1;i>=0;i--){
+	// 		if($scope.todos[i].title==title){
+	// 			$scope.todos[i].completed=false;
+	// 		}
+	// 	}
+	// };
+		
+
+//$scope.message="Hello";
+});
+
+	
+
